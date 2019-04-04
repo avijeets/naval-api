d148eb388547db2a2ed0621f3721e79151f0696c
avijeet <a@avijeets.com>
2019-04-03 22:54:21 -0400

    methods for querying/sending quotes

---
+++ query.js
@@ -0,0 +1,15 @@
+// require all quote collections
+const quotes = require('/quotes/getting-rich.json');
+
+function allQuotes() {
+    return quotes;
+}
+
+function randomQuote() {
+    return quotes[Math.random() * quotes.length];
+}
+
+module.exports = {
+    allQuotes,
+    randomQuote
+};
